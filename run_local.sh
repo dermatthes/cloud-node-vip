37eee87f2463e8ba8808dd363bed6bbe4cd8c780
matthes <leuffen@continue.de>
2017-03-10 18:24:55 +0100

    Changed IP

--- run_local.sh
+++ run_local.sh
@@ -4,6 +4,6 @@
 docker rm -t cloud-node-vip
 docker build -t cloud-node-vip .
 echo "Starting image in interactive mode...";
-docker run --cap-add=NET_ADMIN --net=host -e KEEPALIVED_PRIORITY=5 -it cloud-node-vip
+docker run --cap-add=NET_ADMIN --net=host -e KEEPALIVED_PRIORITY=5 -e VIP_IP=10.19.0.5/24 -e VIP_GW=10.19.0.1 -it cloud-node-vip
 
 echo "Image closed...";
